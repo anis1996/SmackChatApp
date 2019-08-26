d6666976197ff3aa3cca915661e792c3f135c9b3
Anis Dhapa <anis.dhapa@sfgov.org>
2019-08-26 13:20:43 -0700

    Add Login Activity

---
+++ app/src/main/java/dhapa/anis/smackchatapp/LoginActivity.kt
@@ -0,0 +1,23 @@
+package dhapa.anis.smackchatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+    fun loginSignupBtnClicked(view: View)
+    {
+
+    }
+
+    fun loginLoginBtnClicked(view: View){
+
+    }
+}
--- app/src/main/java/dhapa/anis/smackchatapp/MainActivity.kt
+++ app/src/main/java/dhapa/anis/smackchatapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package dhapa.anis.smackchatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -35,7 +36,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNevClicked(view: View){
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelBtnClicked(view: View)
